e4cdd1cbdef667d00b1c9e28c4d6cc1236b4552b
hnhan2003 <ngohoangnhan03@gmail.com>
2024-03-19 10:38:27 +0700

    asf

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("Hello world!");
+console.log("Hello my friend!");
+console.log("This is my friend");
+console.log("how about that");
--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 console.log("This is test file");
 alert("Hello, world!");
+class test {
+  constructor() {
+    console.log("This is test file");
+  }
+}
